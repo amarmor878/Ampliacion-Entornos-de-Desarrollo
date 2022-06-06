d4e7378a5b7a701add19613aa0b4f99b073e63e5
Alberto <amarmor878@g.educaand.es>
2022-06-06 14:33:46 +0200

    Comentada la clase enumerada

---
+++ src/modelo/POJO/Identificar.java
@@ -0,0 +1,13 @@
+package modelo.POJO;
+
+/**
+ * Clase enumerada que identifica el acceso a la aplicación del UsuarioRegistrado y que permite identificarlo
+ * mediante un nick o un apodo.
+ *
+ * @author Alberto
+ * @version 1.0
+ * @created 06-jun-2022
+ */
+public enum Identificar {
+    NICK, APODO;
+}
