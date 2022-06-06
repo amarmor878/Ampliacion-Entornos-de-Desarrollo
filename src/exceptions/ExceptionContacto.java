3046921c40d5ccd65ed4f92fd7033b5959128551
Alberto <amarmor878@g.educaand.es>
2022-06-06 14:33:11 +0200

    Creción de las Excepciones de Usuario y Contacto

---
+++ src/exceptions/ExceptionContacto.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+/**
+ * Clase ExceptionContacto que gestiona las excepciones de la clase Contacto.
+ *
+ * @author Alberto
+ * @version 1.0
+ * @created 06-jun-2022
+ */
+public class ExceptionContacto extends Exception {
+    /**
+     * Constructor de la clase ExceptionContacto.
+     *
+     * @param mensaje String con el mensaje de error.
+     */
+    public ExceptionContacto(String mensaje) {
+        super(mensaje);
+    }
+
+}
---
+++ src/exceptions/ExceptionUsuario.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+/**
+ * ExceptionUsuario es una excepción que se lanza cuando se intenta crear un usuario con un email o password no válidos.
+ *
+ * @author Alberto
+ * @version 1.0
+ * @created 06-jun-2022
+ */
+public class ExceptionUsuario extends Exception {
+    /**
+     * Constructor de la clase ExceptionUsuario.
+     *
+     * @param mensaje String con el mensaje de error.
+     */
+    public ExceptionUsuario(String mensaje) {
+        super(mensaje);
+    }
+}
